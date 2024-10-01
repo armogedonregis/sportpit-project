09e8dfc323d829cdb9b725a70c0c095a90486b3b
armogedonregis <armag171691@gmail.com>
2024-10-01 15:50:08 +0300

    fixed bg header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -45,7 +45,7 @@ export const Header = () => {
 
 
     return (
-        <header className={`bg-black text-white p-4 w-full z-50 top-0 ${scrollY > 0 ? 'fixed' : ''}`}>
+        <header className={`p-4 w-full z-50 transition-colors duration-300 ${scrollY > 0 ? 'fixed top-0' : ''} ${isScrolled ? 'bg-black text-white' : 'bg-white text-black'}`}>
             <div className="lg:max-w-[2560px] w-full px-5 lg:px-12 mx-auto flex justify-between items-center">
                 <Link href="/" className="flex items-center h-[75px]">
                     {!isScrolled ? (
@@ -72,8 +72,8 @@ export const Header = () => {
                                 <Link href="/">
                                     <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" fill="none" aria-hidden="true">
                                         <g>
-                                            <path d="M19 6C19 8.20914 17.2091 10 15 10C12.7909 10 11 8.20914 11 6C11 3.79086 12.7909 2 15 2C17.2091 2 19 3.79086 19 6Z" stroke="white"></path>
-                                            <path d="M15 12C11.134 12 8 15.1091 8 18.9443V26.5H22V18.9443C22 15.1091 18.866 12 15 12Z" stroke="white"></path>
+                                            <path d="M19 6C19 8.20914 17.2091 10 15 10C12.7909 10 11 8.20914 11 6C11 3.79086 12.7909 2 15 2C17.2091 2 19 3.79086 19 6Z" stroke="currentColor"></path>
+                                            <path d="M15 12C11.134 12 8 15.1091 8 18.9443V26.5H22V18.9443C22 15.1091 18.866 12 15 12Z" stroke="currentColor"></path>
                                         </g>
                                     </svg>
                                 </Link>
@@ -84,23 +84,22 @@ export const Header = () => {
                             <div className="relative group">
                                 <Link href="/cart" className="mr-4 relative block">
                                     <svg className="header-icon header-icon-full" width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" fill="none">
-                                        <path d="M22 9.5C22 5.35786 18.6421 2 14.5 2C10.3579 2 7 5.35786 7 9.5" stroke="white"></path>
-                                        <path d="M2 10L5 26.5H24L27 10" stroke="white"></path>
+                                        <path d="M22 9.5C22 5.35786 18.6421 2 14.5 2C10.3579 2 7 5.35786 7 9.5" stroke="currentColor"></path>
+                                        <path d="M2 10L5 26.5H24L27 10" stroke="currentColor"></path>
                                     </svg>
-                                    <span className="absolute -top-2 -right-2 bg-white text-black rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                                    <span className={`absolute -top-2 -right-2 ${isScrolled ? 'bg-white text-black' : 'bg-black text-white'} rounded-full w-5 h-5 flex items-center justify-center text-xs`}>
                                         {cartItemCount}
                                     </span>
                                 </Link>
                                 <div className="absolute right-16 top-0 mt-2 bg-gray-800 text-white py-1 px-3 text-xs rounded opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 whitespace-nowrap">
                                     You have {cartItemCount} item{cartItemCount !== 1 ? 's' : ''} in your cart
-
                                 </div>
                             </div>
                         </div>
                     )}
                     <button className="text-2xl relative w-8 h-8 z-50" onClick={toggleMenu}>
-                        <span className={`block absolute h-0.5 w-8 bg-white transform transition duration-300 ease-in-out ${isMenuOpen ? 'rotate-45 translate-y-1.5' : '-translate-y-1.5'}`}></span>
-                        <span className={`block absolute h-0.5 w-8 bg-white transform transition duration-300 ease-in-out ${isMenuOpen ? '-rotate-45' : 'translate-y-1.5'}`}></span>
+                        <span className={`block absolute h-0.5 w-8 bg-current transform transition duration-300 ease-in-out ${isMenuOpen ? 'rotate-45 translate-y-1.5' : '-translate-y-1.5'}`}></span>
+                        <span className={`block absolute h-0.5 w-8 bg-current transform transition duration-300 ease-in-out ${isMenuOpen ? '-rotate-45' : 'translate-y-1.5'}`}></span>
                     </button>
                 </nav>
                 <div className={`bg-black absolute top-0 left-0 w-screen text-white transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-y-0' : '-translate-y-full'}`} style={{ height: '80vh' }}>
