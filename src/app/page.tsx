82ff4afed1d128e341cde6db093ef255ef2473d5
armogedonregis <armag171691@gmail.com>
2024-11-06 22:48:10 +0300

    text-left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <section className="relative bg-white h-[416px] lg:min-h-screen flex flex-col justify-between p-6 md:p-8 lg:p-16 bg-center bg-cover bg-no-repeat" style={{
         backgroundImage: "url('/images/summit/summit_bg.jpg')"
       }}>
-        <h2 className="text-lg md:text-4xl text-center font-bold text-white mb-4 lg:mb-0">THE HIGHEST PROTEIN CONCENTRATE
+        <h2 className="text-lg md:text-4xl font-bold text-white mb-4 lg:mb-0">THE HIGHEST PROTEIN CONCENTRATE
           IN PLANT-BASED NUTRITION-EVER!</h2>
       </section>
     </>
