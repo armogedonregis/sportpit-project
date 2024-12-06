1eb41ae9aa2a4a4561c0ce4c1d27427cf3172433
armogedonregis <armag171691@gmail.com>
2024-12-06 21:25:07 +0300

    fixed uppercase

--- src/components/screens/home_screen/bicarb_system_block.tsx
+++ src/components/screens/home_screen/bicarb_system_block.tsx
@@ -7,7 +7,7 @@ export const BicarbSystemBlock = () => {
     }}>
       <div className="flex flex-row justify-center h-full items-end">
         <div className="flex flex-col items-center gap-3 lg:gap-12">
-          <p className="text-white text-xl md:text-3xl lg:text-4xl font-bold font-jost text-center">We crafted plant-based proteins and responsibly-sourced<br /> supplements for your optimal performance in mind.</p>
+          <p className="text-white text-xl md:text-3xl uppercase lg:text-4xl font-bold font-jost text-center">We crafted plant-based proteins and responsibly-sourced<br /> supplements for your optimal performance in mind.</p>
           <Link href="/mamostong" className="rounded-3xl uppercase font-bold text-lg lg:text-xl bg-black px-16 py-2 hover:opacity-80 transition-all text-[#B9F54F]">
             TAKE THE PATH
           </Link>
