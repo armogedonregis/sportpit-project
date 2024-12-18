4924828274587eb8e47398aa7f3cd2d9be37a047
armogedonregis <armag171691@gmail.com>
2024-12-18 20:43:37 +0300

    test image optimizied

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'mamostong.kolotushins.tech',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https', 
-                hostname: 'mamostong.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-        dangerouslyAllowSVG: true,
-        contentDispositionType: 'attachment',
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    },
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.(glb|gltf)$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    publicPath: '/_next/static/images',
-                    outputPath: 'static/images/',
-                },
-            },
-        })
-        return config
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mamostong.kolotushins.tech",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "mamostong.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    // Использовать Sharp вместо Squoosh
+    loader: "default",
+    // Форматы вывода
+    formats: ["image/avif", "image/webp"],
+    // Настройки качества
+    quality: 75,
+    // Размеры устройств для респонсивных изображений
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // Размеры изображений
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/images",
+          outputPath: "static/images/",
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
